ac3e2dcf8b68ff01017d9ed278a44c4da8feccd3
Efe Özgür <efecuneytozgur@gmail.com>
2018-07-11 00:00:55 +0300

    Masaüstü

--- src/KisiFormu.java
+++ src/KisiFormu.java
@@ -1,4 +1,5 @@
 
+import com.sun.xml.internal.bind.v2.runtime.output.SAXOutput;
 import java.awt.event.KeyEvent;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -48,6 +49,8 @@ public class KisiFormu extends javax.swing.JFrame {
         cmbBes = new javax.swing.JComboBox<>();
         cmbSendika = new javax.swing.JComboBox<>();
         tfIban = new javax.swing.JTextField();
+        lblGVOrani = new javax.swing.JLabel();
+        cmbGVOrani = new javax.swing.JComboBox<>();
         jPanel3 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
@@ -59,6 +62,7 @@ public class KisiFormu extends javax.swing.JFrame {
         cmbToplamGun = new javax.swing.JComboBox<>();
         jPanel4 = new javax.swing.JPanel();
         cmbDonem = new javax.swing.JComboBox<>();
+        lblNet = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Sürekli İşçi Maaş Programı");
@@ -149,6 +153,10 @@ public class KisiFormu extends javax.swing.JFrame {
 
         cmbSendika.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Yok", "Var" }));
 
+        lblGVOrani.setText("Gelir Vergisi Oranı");
+
+        cmbGVOrani.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "15", "20", "27", "30" }));
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -164,9 +172,11 @@ public class KisiFormu extends javax.swing.JFrame {
                     .addComponent(lblCocukSayisi)
                     .addComponent(lblAdSoyad)
                     .addComponent(lblTcNo)
-                    .addComponent(lblMedeniDurum))
+                    .addComponent(lblMedeniDurum)
+                    .addComponent(lblGVOrani))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(cmbGVOrani, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(tfIban)
                     .addComponent(cmbSendika, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(cmbBes, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -199,12 +209,13 @@ public class KisiFormu extends javax.swing.JFrame {
                     .addComponent(cmbMedeniDurum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(cmbCocukSayisi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(19, 19, 19))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(lblCocukSayisi)
-                        .addGap(18, 18, 18)))
+                    .addComponent(cmbCocukSayisi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblCocukSayisi))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(cmbGVOrani, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblGVOrani))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lblAGI)
                     .addComponent(cmbAgi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -303,6 +314,8 @@ public class KisiFormu extends javax.swing.JFrame {
                 .addGap(0, 0, Short.MAX_VALUE))
         );
 
+        lblNet.setText("Net Ödenen : ");
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
@@ -316,7 +329,9 @@ public class KisiFormu extends javax.swing.JFrame {
                         .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(18, 18, 18)
-                .addComponent(btnZemin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnZemin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblNet))
                 .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
@@ -324,13 +339,16 @@ public class KisiFormu extends javax.swing.JFrame {
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnZemin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(btnZemin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(16, 16, 16)
+                        .addComponent(lblNet))
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                     .addComponent(jPanel4, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jPanel3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jPanel3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(16, Short.MAX_VALUE))
         );
 
         getContentPane().add(jPanel2, java.awt.BorderLayout.CENTER);
@@ -414,15 +432,17 @@ public class KisiFormu extends javax.swing.JFrame {
                 hesap.setBrutUcret(4);
                 hesap.setGunlukUcret(gun);
                 hesap.SGK_Matrahi(asy);
-                hesap.gelirVergisi(15, AGILER.EVLI_ESI_CALISAN_2_COCUKLU);
+                hesap.gelirVergisi(Integer.parseInt(cmbGVOrani.getSelectedItem().toString()), cmbAgi.getSelectedIndex());
 
                 System.out.println("SGK %1 : " + hesap.sgk1());
                 System.out.println("SGK %2 : " + hesap.sgk2());
+                System.out.println(hesap.gelirVergisi(Integer.parseInt(cmbGVOrani.getSelectedItem().toString()), cmbAgi.getSelectedIndex()));
+                System.out.println(cmbAgi.getSelectedIndex());
 
-                System.out.println(asy.getCocukYardimi());
+                lblNet.setText("Net Ödenen : " + String.valueOf(hesap.netOdenen(asy, ysy)));
                 System.out.println("Hakediş Toplamı : "+hesap.hakedisToplami(asy, ysy));
                 System.out.println("Hakediş Toplamı : " +hesap.netOdenen(asy, ysy));
-
+/*
                 Class.forName("org.sqlite.JDBC");
                 String url = "jdbc:sqlite:db\\db.db";
                 c = DriverManager.getConnection(url);
@@ -442,7 +462,7 @@ public class KisiFormu extends javax.swing.JFrame {
                 psmt.executeUpdate();
                 stmt.close();
                 c.commit();
-                c.close();
+                c.close();*/
             } catch (Exception e) {
                 System.out.println(e);
             }
@@ -527,6 +547,7 @@ public class KisiFormu extends javax.swing.JFrame {
     private javax.swing.JComboBox<String> cmbBes;
     private javax.swing.JComboBox<String> cmbCocukSayisi;
     private javax.swing.JComboBox<String> cmbDonem;
+    private javax.swing.JComboBox<String> cmbGVOrani;
     private javax.swing.JComboBox<String> cmbHaftaTatili;
     private javax.swing.JComboBox<String> cmbMedeniDurum;
     private javax.swing.JComboBox<String> cmbNormalGun;
@@ -546,8 +567,10 @@ public class KisiFormu extends javax.swing.JFrame {
     private javax.swing.JLabel lblAdSoyad;
     private javax.swing.JLabel lblBes;
     private javax.swing.JLabel lblCocukSayisi;
+    private javax.swing.JLabel lblGVOrani;
     private javax.swing.JLabel lblIban;
     private javax.swing.JLabel lblMedeniDurum;
+    private javax.swing.JLabel lblNet;
     private javax.swing.JLabel lblSendika;
     private javax.swing.JLabel lblTcNo;
     private javax.swing.JLabel lblUnvan;
