518e553214dd83408368cbebe96701dd28a898a0
Efe Özgür <efecuneytozgur@gmail.com>
2018-07-11 23:32:53 +0300

    Masaüstü

--- src/Bordro.java
+++ src/Bordro.java
@@ -34,17 +34,17 @@ public class Bordro extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null}
+                {null, null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null, null}
             },
             new String [] {
-                "Adı Soyadı", "TC Kimlik No", "Medeni Durumu", "Brüt Ücret", "Günlük Ücret", "Yol Yardımı", "Yemek Yardımı"
+                "Adı Soyadı", "TC Kimlik No", "Medeni Durumu", "Brüt Ücret", "Günlük Ücret", "Yol Yardımı", "Yemek Yardımı", "Çocuk Yardımı", "Yakacak Yardımı", "Bayram Yardımı"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Double.class, java.lang.String.class, java.lang.Double.class, java.lang.Double.class
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Double.class, java.lang.String.class, java.lang.Double.class, java.lang.Double.class, java.lang.Double.class, java.lang.Double.class, java.lang.Double.class
             };
 
             public Class getColumnClass(int columnIndex) {
